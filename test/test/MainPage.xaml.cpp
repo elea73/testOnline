c980989e3b52bd4a8d5adf6454c1cca9c7a3f9e0
Nicolas_Detienne <nicolas.detienne@hotmail.fr>
2015-01-31 16:17:33 +0100

    test

---
+++ test/test/MainPage.xaml.cpp
@@ -0,0 +1,100 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+using namespace test;
+
+using namespace Platform;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Graphics::Display;
+using namespace Windows::Phone::UI::Input;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+
+// The WebView Application template is documented at http://go.microsoft.com/fwlink/?LinkID=391641
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+
+	this->NavigationCacheMode = Navigation::NavigationCacheMode::Required;
+}
+
+void MainPage::OnNavigatedTo(NavigationEventArgs^ e)
+{
+	(e);	// Unused parameter
+
+	WebViewControl->Navigate(GetHomeUri());
+
+	_backPressedToken = HardwareButtons::BackPressed += ref new EventHandler<BackPressedEventArgs^>(this, &MainPage::MainPage_BackPressed);
+}
+
+void MainPage::OnNavigatedFrom(NavigationEventArgs^ e)
+{
+	(e);	// Unused parameter
+
+	HardwareButtons::BackPressed -= _backPressedToken;
+}
+
+void MainPage::Browser_NavigationCompleted(Object^ sender, WebViewNavigationCompletedEventArgs^ e)
+{
+	(sender);	// Unused parameter
+	(e);		// Unused parameter
+	
+#if _DEBUG
+	if (!e->IsSuccess)
+	{
+		OutputDebugStringW(L"Navigation to this page failed, check your internet connection");
+	}
+#endif
+}
+
+/// <summary>
+/// Overrides the back button press to navigate in the WebView's back stack instead of the application's.
+/// </summary>
+void MainPage::MainPage_BackPressed(Object^ sender, BackPressedEventArgs^ e)
+{
+	(sender);	// Unused parameter
+	(e);		// Unused parameter
+
+	if (WebViewControl->CanGoBack)
+	{
+		WebViewControl->GoBack();
+		e->Handled = true;
+	}
+}
+
+/// <summary>
+/// Navigates forward in the WebView's history.
+/// </summary>
+void MainPage::ForwardAppBarButton_Click(Object^ sender, RoutedEventArgs^ e)
+{
+	(sender);	// Unused parameter
+	(e);		// Unused parameter
+
+	if (WebViewControl->CanGoBack)
+	{
+		WebViewControl->GoForward();
+	}
+}
+
+/// <summary>
+/// Navigates to the initial home page.
+/// </summary>
+void MainPage::HomeAppBarButton_Click(Object^ sender, RoutedEventArgs^ e)
+{
+	(sender);	// Unused parameter
+	(e);		// Unused parameter
+
+	WebViewControl->Navigate(GetHomeUri());
+}
---
+++ test/test/MainPage.xaml.h
@@ -0,0 +1,35 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+namespace test
+{
+	public ref class MainPage sealed
+	{
+		Windows::Foundation::Uri^ GetHomeUri() const
+		{
+			// TODO: Replace with your URL here.
+			return ref new Windows::Foundation::Uri(L"ms-appx-web:///Html/index.html");
+		}
+
+	public:
+		MainPage();
+
+	protected:
+		virtual void OnNavigatedTo(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+		virtual void OnNavigatedFrom(Windows::UI::Xaml::Navigation::NavigationEventArgs^ e) override;
+
+	private:
+		Windows::Foundation::EventRegistrationToken _backPressedToken;
+
+		void Browser_NavigationCompleted(Platform::Object^ sender, Windows::UI::Xaml::Controls::WebViewNavigationCompletedEventArgs^ e);
+		void MainPage_BackPressed(Platform::Object^ sender, Windows::Phone::UI::Input::BackPressedEventArgs^ e);
+		void ForwardAppBarButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+		void HomeAppBarButton_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
